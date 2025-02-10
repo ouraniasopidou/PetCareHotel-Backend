8a882fb3a5aa4f323f0f8a9a9cc9cf3406f39731
ouraniasopidou <rania_sg@hotmail.com>
2025-02-11 00:15:35 +0200

    Updated application.properties to use environment variables

--- src/main/java/gr/so/thepethotelapp/ThepethotelAppApplication.java
+++ src/main/java/gr/so/thepethotelapp/ThepethotelAppApplication.java
@@ -1,5 +1,6 @@
 package gr.so.thepethotelapp;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ThepethotelAppApplication {
 
     public static void main(String[] args) {
+        // Φόρτωση μεταβλητών από το .env
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+        // Εκκίνηση της εφαρμογής Spring Boot
         SpringApplication.run(ThepethotelAppApplication.class, args);
     }
 }
